32b230b76e08d4034d6027fbf322c9ce3030e155
Tony Corbett <tonyc.uk@gmail.com>
2017-06-21 22:28:22 +0100

    Add GPL v3 header to script

--- bt_login.sh
+++ bt_login.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+# bt_login.sh - A simple shell script to automate BT FON logins.
+# Copyright (C) 2017  Tony Corbett, G0WFV
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 user=UsErNaMe
 pass=PaSsWoRd
 
