9a000736bd11bab89808421a0d232e154c879245
Tony Corbett, G0WFV <tonyc.uk@gmail.com>
2017-06-24 23:52:55 +0100

    Change STDOUT to logging to syslog & fix running from CRON

--- bt_login.sh
+++ bt_login.sh
@@ -21,42 +21,43 @@ pass=PaSsWoRd
 
 shopt -s extglob         # enables pattern lists like +(...|...)
 listOfESSID='+(BTFON|BTOpenzone|BTOpenzone-B|BTOpenzone-H|BTWi-fi|BTWifi|BTWiFI-with-FON|BTWifi-with-FON|_BTWi-fi)'
-currentESSID=$(iwgetid | sed 's/.*\"\(.*\)\".*/\1/g')
+currentESSID=$(/sbin/iwgetid | sed 's/.*\"\(.*\)\".*/\1/g')
+processName=$(echo $0 | /bin/sed 's/.*\/\(.*\)/\1/g')
+pid=$$
 
-foo=$(wget "https://www.btopenzone.com:8443/home" --no-check-certificate --no-cache --timeout 30 -O - 2>/dev/null)
+foo=$(/usr/bin/wget "https://www.btopenzone.com:8443/home" --no-check-certificate --no-cache --timeout 30 -O - 2>/dev/null)
 
 if [ $? -ne 0 ]
 then
-	echo "There isn't an internet connection. Exiting."
+	/usr/bin/logger -t $processName --id=$pid "There isn't an internet connection. Exiting."
 	exit $?
 fi
 
-loggedIn=$(echo $foo | grep 'now logged on to BT Wi-fi')
+loggedIn=$(echo $foo | /bin/grep 'now logged on to BT Wi-fi')
 
 if [ $? -eq 0 ]
 then
-	echo "You're already logged in. Nowt to do!"
+	/usr/bin/logger -t $processName --id=$pid "You're already logged in. Nowt to do!"
 	exit 0
 else
-	echo -n "$currentESSID "
 	case "$currentESSID" in
 		$listOfESSID)
-			echo -n "is a valid Wifi network. Logging in ... "
-			foo=$(wget -qO - --no-check-certificate --no-cache --post-data "username=$user&password=$pass" "https://www.btopenzone.com:8443/tbbLogon")
-			loggedIn=$(echo $foo | grep 'now logged on to BT Wi-fi')
+			/usr/bin/logger -t $processName --id=$pid "$currentESSID is a valid Wifi network. Logging in ... "
+			foo=$(/usr/bin/wget -qO - --no-check-certificate --no-cache --post-data "username=$user&password=$pass" "https://www.btopenzone.com:8443/tbbLogon")
+			loggedIn=$(echo $foo | /bin/grep 'now logged on to BT Wi-fi')
 
 			if [ $? -eq 0 ]
 			then
-				echo "Success!"
+				/usr/bin/logger -t $processName --id=$pid "Success!"
 				exit 0
 			else
-				echo "Oops!"
+				/usr/bin/logger -t $processName --id=$pid "Oops!"
 				exit 1
 			fi
 		;;
 
 		*)
-			echo "is not in the list of valid Wifi networks."
+			/usr/bin/logger -t $processName --id=$pid "$currentESSID is not in the list of valid Wifi networks."
 			exit 1
 	esac
 fi
